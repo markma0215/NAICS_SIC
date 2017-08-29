08f0bf16ae74d556b8f71459ceedab1634985289
markma <maxiao0215@gmail.com>
2017-08-29 01:53:43 -0400

    *****

--- config.py
+++ config.py
@@ -2,9 +2,9 @@
 # matched = 228
 # unmatched = 2980
 # counter = 3209
-matched = 1103
-unmatched = 3060
-counter = 4164
+matched = 1334
+unmatched = 3633
+counter = 4968
 url_base = "http://siccode.com/en/search/"
 
 # search_result
--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def file_writer_unmatched(str_list):
 
 
 def requestHTMLPage(url):
-    time.sleep(random.randint(1, 5))
+    time.sleep(random.randint(1, 3))
     status = False
     times = 0
     html_page = ""
